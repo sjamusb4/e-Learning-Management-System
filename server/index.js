4960d332f4486e1dceb8bfbf012bf8825aae04f0
Sourabh Jamdade <sourabh.vishnu-jamdade@capgemini.com>
2026-05-23 13:50:43 +0530

    added routes to app

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express();
 require("dotenv").config();
 const { userRouter } = require("./routes/user.route");
 const { moduleRouter } = require("./routes/module.route");
+const { lessonRouter } = require("./routes/lesson.route");
+const { enrollmentRouter } = require("./routes/enrollment.route");
+const { lessonProgressRouter } = require("./routes/lessonProgress.route");
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +13,9 @@ app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/module", moduleRouter);
+app.use("/api/lesson", lessonRouter);
+app.use("/api/enrollment", enrollmentRouter);
+app.use("/api/lesson-progress", lessonProgressRouter);
 
 app.get("/", (req, res) => {
   res.send({
